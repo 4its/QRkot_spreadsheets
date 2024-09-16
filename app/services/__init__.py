85e51aa6b2202a7e9b5dee2094719af3071ac36f
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-09-16 11:35:57 +0300

    add func for easy import;

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,1 @@
-from .google_api import prepare_data, set_user_permissions, spreadsheets_create, spreadsheets_update_value # noqa
+from .google_api import format_time_in_days, prepare_data, set_user_permissions, spreadsheets_create, spreadsheets_update_value # noqa
